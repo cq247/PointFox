f7721b9ff3092e67f36bca44815138c2dbd5b1bf
cq247   <2219197543@qqcom>
2021-05-06 09:47:29 +0800

    '增加public、src目录'

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="footerWrapper">
+    <div id="footerTip1">
+      <span>
+        通讯地址：江西省南昌市湾里区罗亭镇江西软件大学
+      </span>
+      </div>
+     <div id="footerTip2">
+       <span>
+        版权信息：&copy; 版权归江西软件大学所有
+      </span>
+       </div> 
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      user:{
+        username:"",
+        password:""
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#footerWrapper{
+  text-align: center;
+}
+</style>
